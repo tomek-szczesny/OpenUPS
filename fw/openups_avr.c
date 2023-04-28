a80ff6592df69fbf61706767a183b291409040dd
Tomek Szczęsny <mctom@tlen.pl>
2023-04-28 20:53:23 +0200

    A buildable AVR FW template

---
+++ fw/openups_avr.c
@@ -0,0 +1,27 @@
+// -----------------------------------------------------------------------------
+// AVR microcontroller code
+// A part of OpenUPS Project
+// Website: https://github.com/tomek-szczesny/OpenUPS
+// Authors: Tomek Szczęsny
+// License: TBD
+// -----------------------------------------------------------------------------
+
+#include <avr/io.h>
+//#include <avr/interrupt.h>
+#include <avr/sleep.h>
+
+
+/* Interrupt handlers */
+
+void init (void) {	/* Initialization routine */
+
+}
+
+int main (void)
+{
+    init();
+    while (1) {            /* Main Loop */
+
+    }
+    return (0);
+}
