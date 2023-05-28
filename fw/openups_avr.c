33977c01b03ea5d80257fe4b7ec99c65ee519f99
Edward Kisiel <hominoid@cablemi.com>
2023-05-27 21:35:26 -0400

    added twi.c, twi.h AVR Dx I2C initialization routine that supports both TWI0 and TWI1 in either host or client mode.

--- fw/openups_avr.c
+++ fw/openups_avr.c
@@ -10,7 +10,7 @@
 #include <avr/interrupt.h>
 #include <avr/sleep.h>
 #include <util/delay.h>
-
+#include "twi.h"
 
 /* Interrupt handlers */
 
@@ -21,12 +21,28 @@ void init (void) {	/* Initialization routine */
 int main (void)
 {
     init();
+    // setup i2c port 0 as host
+    if(twi_init(TWI_MODE_HOST, TWI0_I2C, TWI0_BAUD)) error(TWI_ERROR_NOHOST);
     PORTC_DIRSET = 0x40;
-    while (1) {            /* Main Loop */
-        PORTC_OUTSET = 0x40;
-        _delay_ms(100);
-        PORTC_OUTCLR = 0x40;
-        _delay_ms(100);
+    if(TWI0_MSTATUS > 0) {
+        for (int i = 0; i <= TWI0_MSTATUS; i++) {
+            PORTC_OUTCLR = 0x40;
+            _delay_ms(50);
+            PORTC_OUTSET = 0x40;
+            _delay_ms(50);
+        }
+    }
+    _delay_ms(500);
+    // setup i2c port 1 as client
+    if(twi_init(TWI_MODE_CLIENT, TWI1_I2C, TWI1_BAUD)) error(TWI_ERROR_NOHOST);
+    PORTC_DIRSET = 0x40;
+    if(TWI1_MSTATUS > 0) {
+        for (int i = 0; i <= TWI1_MSTATUS; i++) {
+            PORTC_OUTCLR = 0x40;
+            _delay_ms(50);
+            PORTC_OUTSET = 0x40;
+            _delay_ms(50);
+        }
     }
     return (0);
 }
